3aa499f478ef3e6b52e7b7c1682d89e6f9e52697
toshi0607 <s.toshi0607@gmail.com>
2018-05-12 21:17:16 +0900

    :bug: file path

--- script/test.sh
+++ script/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -eu
 
+SCRIPTS=$(cd $(dirname $0) && pwd)
+
 echo "" > coverage.txt
-for d in $(${MYDIR}/packages.sh); do
+for d in $(${SCRIPTS}/packages.sh); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt

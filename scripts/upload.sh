3cdcd78bb507a029e9bc9d4ed2dbff17a1538ebb
toshi0607 <s.toshi0607@gmail.com>
2018-05-13 10:20:16 +0900

    Checking in changes prior to tagging of version v0.0.2

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Usage: GITHUB_TOKEN=... script/release
-set -e
+set -eu
+
+# set GITHUB_TOKEN=...
+
 latest_tag=$(git describe --abbrev=0 --tags)
 goxc
 ghr -u toshi0607 -r gig $latest_tag dist/snapshot/

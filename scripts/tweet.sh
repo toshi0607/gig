c8d94c475fd698ebe6fcbc4dbb7d5c64400d3432
toshi0607 <s.toshi0607@gmail.com>
2018-06-09 14:11:51 +0900

    :bug: improve to tweet

--- scripts/tweet.sh
+++ scripts/tweet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-curl -X POST -d '{"owner": "toshi0607", "repo": "release-tweeter"}' ${RELEASE_TWEETER_ENDPOINT}
+curl -X POST -d '{"owner": "toshi0607", "repo": "gig"}' ${RELEASE_TWEETER_ENDPOINT}
